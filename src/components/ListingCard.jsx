2eca69b1bdffabd5be775faf6ff2bd1599c19bf7
kaburbank <kaburbank@gmail.com>
2026-01-09 09:56:29 -0700

    Enhance ListingCard with favorite and delete features
    Added functionality to handle favorite and delete actions for listings.

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,20 +1,46 @@
-function ListingCard({ description, image, location, favorite }) {
+​​function ListingCard({ id, image, description, location, favorite, updateListing }) {
+
+  const handleFavorite = () => {
+    fetch(`http://localhost:6001/listings${id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({favorite: !favorite})
+    })
+      .then(r => {
+        if (!r.ok) {throw new Error("failed to favorite listing") }
+          return r.json()
+        })
+      .then(updateListing)
+      .catch(error => console.log(error.message))
+  }
+
+  // add fetch request
+  const handleDelete = () => {
+    fetch(`http://localhost:6001/listings${id}`, {
+            method: "DELETE"
+    })
+      .then(r => {
+          if (!r.ok) {throw new Error("failed to favorite listing") }
+          console.log("Item deleted")
+        })
+      .catch(error => console.log(error.message))
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        { // add description and image based on prop value }
         <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {favorite ? (
+          <button onClick={handleFavorite} className="emoji-button favorite active">★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button onClick={handleFavorite} className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button onClick={handleDelete} className="emoji-button delete">🗑</button>
       </div>
     </li>
   );
